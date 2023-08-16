1aca435aa24b78c7c580a4b346109dc879e1d9bf
johmacher-pccl <johannes.macher@pccl.at>
2023-08-16 10:13:03 +0200

    first commit

---
+++ Advent_of_Code__2015__01.py
@@ -0,0 +1,7 @@
+# =========================================================================================
+# The file Advent_of_Code__2015__01.py was created by Johannes Macher
+# @ Polymer Competence Center Leoben GmbH on the 2023-08-16
+# =========================================================================================
+
+# Imports
+import numpy as np
---
+++ README.md
@@ -0,0 +1,2 @@
+# My GitHub Project for "Advent of Code"
+This is my GitHub project for "Advent of Code" (url: https://adventofcode.com/2021/events).
