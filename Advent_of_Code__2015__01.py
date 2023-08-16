2878900afc16f6780b7fa40f550c0764ffe897ba
johmacher-pccl <johannes.macher@pccl.at>
2023-08-16 12:06:49 +0200

    Completed Day 2, Combined Parts 1 & 2

--- Advent_of_Code__2015__01.py
+++ Advent_of_Code__2015__01.py
@@ -3,7 +3,7 @@
 # @ Polymer Competence Center Leoben GmbH on the 2023-08-16
 # =========================================================================================
 
-# Task 01
+# Task 01 - Part 1
 """
 Santa is trying to deliver presents in a large apartment building, but he can't find the right floor - the directions he got are a little confusing. 
 He starts on the ground floor (floor 0) and then follows the instructions one character at a time.
@@ -22,6 +22,20 @@ For example:
 
 """
 
+# Task 01 - Part 2
+"""
+Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). 
+The first character in the instructions has position 1, the second character has position 2, and so on.
+
+For example:
+
+    ) causes him to enter the basement at character position 1.
+    ()()) causes him to enter the basement at character position 5.
+
+What is the position of the character that causes Santa to first enter the basement?
+
+"""
+
 # Imports
 import os
 import numpy as np
@@ -32,8 +46,30 @@ load_path = os.path.join(home_directory, "Data_2015", "Data__Advent_of_Code__201
 
 santa_floor_string = str(np.loadtxt(load_path, delimiter=None, dtype=str))
 
+# ----------------------------------------------------------------------------------------------------------------------
+# Part 1
 # Count characters
 floor_number = santa_floor_string.count("(") - santa_floor_string.count(")")
 
 # Print Floor
+print("\n")
 print("Floor Number: ", floor_number)
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Part 2
+# Calculate the number of string which puts Santa in the basement (-1)
+santa_floor_list = list(santa_floor_string)
+
+current_floor = 0
+for idx, item in enumerate(santa_floor_list):
+    if item == "(":
+        current_floor += 1
+    else:
+        current_floor -= 1
+
+    if current_floor == -1:
+        break
+
+# Print character which causes Santa to be in the basement (-1)
+print(f"Character {(idx + 1):d} puts Santa in the basement.")
+print("\n")
--- Advent_of_Code__2015__02.py
+++ Advent_of_Code__2015__02.py
@@ -3,18 +3,37 @@
 # @ Polymer Competence Center Leoben GmbH on the 2023-08-16
 # =========================================================================================
 
-# Task 02
+# Task 02 - Part 1
 """
-Now, given the same instructions, find the position of the first character that causes him to enter the basement (floor -1). The first character in the instructions has position 1, the second character has position 2, and so on.
+The elves are running low on wrapping paper, and so they need to submit an order for more. They have a list of the dimensions (length l, width w, and height h)
+of each present, and only want to order exactly as much as they need.
+
+Fortunately, every present is a box (a perfect right rectangular prism), which makes calculating the required wrapping paper for each gift a little easier:
+find the surface area of the box, which is 2*l*w + 2*w*h + 2*h*l. The elves also need a little extra paper for each present: the area of the smallest side.
 
 For example:
 
-    ) causes him to enter the basement at character position 1.
-    ()()) causes him to enter the basement at character position 5.
+    A present with dimensions 2x3x4 requires 2*6 + 2*12 + 2*8 = 52 square feet of wrapping paper plus 6 square feet of slack, for a total of 58 square feet.
+    A present with dimensions 1x1x10 requires 2*1 + 2*10 + 2*10 = 42 square feet of wrapping paper plus 1 square foot of slack, for a total of 43 square feet.
 
-What is the position of the character that causes Santa to first enter the basement?
-
+All numbers in the elves' list are in feet. How many total square feet of wrapping paper should they order?
 """
+
+# Task 02 - Part 2
+"""
+The elves are also running low on ribbon. Ribbon is all the same width, so they only have to worry about the length they need to order, 
+which they would again like to be exact.
+
+The ribbon required to wrap a present is the shortest distance around its sides, or the smallest perimeter of any one face. Each present 
+also requires a bow made out of ribbon as well; the feet of ribbon required for the perfect bow is equal to the cubic feet of volume of the present. 
+Don't ask how they tie the bow, though; they'll never tell.
+
+For example:
+
+    A present with dimensions 2x3x4 requires 2+2+3+3 = 10 feet of ribbon to wrap the present plus 2*3*4 = 24 feet of ribbon for the bow, for a total of 34 feet.
+    A present with dimensions 1x1x10 requires 1+1+1+1 = 4 feet of ribbon to wrap the present plus 1*1*10 = 10 feet of ribbon for the bow, for a total of 14 feet.
+
+How many total feet of ribbon should they order?"""
 
 # Imports
 import os
@@ -22,22 +41,36 @@ import numpy as np
 
 # read in string from data
 home_directory = os.path.dirname(__file__)
-load_path = os.path.join(home_directory, "Data_2015", "Data__Advent_of_Code__2015__01.txt")
+load_path = os.path.join(home_directory, "Data_2015", "Data__Advent_of_Code__2015__02.txt")
 
-santa_floor_string = str(np.loadtxt(load_path, delimiter=None, dtype=str))
+dimensions_array = np.loadtxt(load_path, delimiter=None, dtype=str).tolist()
+for idx, item in enumerate(dimensions_array):
+    dimensions_array[idx] = item.split("x")
+dimensions_array = np.array(dimensions_array).astype(float)
 
-# Calculate the number of string which puts Santa in the basement (-1)
-santa_floor_list = list(santa_floor_string)
+# ----------------------------------------------------------------------------------------------------------------------
+# Part 1
+# Calculate area of wrapping paper
+wrapping_paper_area = 0.0
+for dimensions in dimensions_array:
+    area1 = dimensions[0] * dimensions[1]
+    area2 = dimensions[0] * dimensions[2]
+    area3 = dimensions[1] * dimensions[2]
 
-current_floor = 0
-for idx, item in enumerate(santa_floor_list):
-    if item == "(":
-        current_floor += 1
-    else:
-        current_floor -= 1
+    wrapping_paper_area += 2.0 * (area1 + area2 + area3) + np.min([area1, area2, area3])
 
-    if current_floor == -1:
-        break
+# Total area to order
+print("\n")
+print(f"Total area of wrapping paper to order: {wrapping_paper_area:.0f}")
 
-# Print character which causes Santa to be in the basement (-1)
-print(f"Character {(idx + 1):d} puts Santa in the basement.")
+# ----------------------------------------------------------------------------------------------------------------------
+# Part 2
+# Calculate ribbon length
+ribbon_length = 0.0
+for dimensions in dimensions_array:
+    idx = np.argwhere(dimensions == np.max(dimensions))[0, 0]
+    ribbon_dimension = np.delete(dimensions, idx)
+    ribbon_length += 2.0 * np.sum(ribbon_dimension) + np.prod(dimensions)
+
+print(f"Total length of ribbon to order: {ribbon_length:.0f}")
+print("\n")
